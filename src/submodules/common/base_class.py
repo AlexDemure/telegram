5781db73fe756c20109a91fe5b78970d7df83e5a
AlexDemure <alexanderdemure@gmail.com>
2021-03-05 14:36:22 +0500

    tenacity

--- src/submodules/common/base_class.py
+++ src/submodules/common/base_class.py
@@ -1,3 +1,5 @@
+from tenacity import retry, wait_exponential, stop_after_attempt
+
 
 class BaseClass:
 
@@ -15,12 +17,22 @@ class APIClass(BaseClass):
         super().__init__(http_client)
         self.headers = {"Authorization": auth_token}
 
+    @retry(
+        wait=wait_exponential(multiplier=1, min=1, max=2),
+        stop=stop_after_attempt(3),
+        reraise=True,
+    )
     async def _rs_get(self, url) -> dict:
         response = await self.client.get(url=url, headers=self.headers)
         assert response.status_code == 200
 
         return response.json()
 
+    @retry(
+        wait=wait_exponential(multiplier=1, min=1, max=2),
+        stop=stop_after_attempt(3),
+        reraise=True,
+    )
     async def _rs_post(self, url, data: dict = None) -> dict:
         response = await self.client.post(url=url, headers=self.headers, json=data)
         assert response.status_code == 200
