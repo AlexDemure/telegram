658ac7d1aec6dea1a64a1fc9d938a690c3118f6b
kirkylesich <stealer-mail@mail.ru>
2021-03-12 19:14:38 +0500

    docker-compose fixed

--- src/core/application.py
+++ src/core/application.py
@@ -47,11 +47,3 @@ async def application():
 
     return app
 
-
-if __name__ == '__main__':
-    import os
-    ENV = os.environ.get("ENV", "DEV")
-    if ENV == "PROD":
-        os.system("gunicorn src.core.application:application --bind 127.0.0.1:7040 --worker-class aiohttp.GunicornWebWorker")
-    else:
-        web.run_app(application(), host="127.0.0.1", port=7040)
