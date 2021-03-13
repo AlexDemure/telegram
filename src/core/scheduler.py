0ba7b49bf1e0289760ea948e3f4fa7bd9b1fd95d
AlexDemure <alexanderdemure@gmail.com>
2021-03-13 22:44:06 +0500

    scheduler

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -7,7 +7,7 @@ from src.apps.hubstaff.tasks import daily_send_today_time_tracked_and_activity
 def start():
     """Запуск событий по Cron времени."""
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-    scheduler.add_job(daily_send_list_tasks, 'cron', hour=9)
-    scheduler.add_job(daily_send_list_tasks_with_unset_time, 'cron', hour=9)
-    scheduler.add_job(daily_send_today_time_tracked_and_activity, 'cron', hour=20)
+    scheduler.add_job(daily_send_list_tasks, 'cron', hour=9, day_of_week='0-5')
+    scheduler.add_job(daily_send_list_tasks_with_unset_time, 'cron', hour=9, day_of_week='0-5')
+    scheduler.add_job(daily_send_today_time_tracked_and_activity, 'cron', hour=20, day_of_week='0-5')
     scheduler.start()
